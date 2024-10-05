5d40c9a5dd03901f3a7ef397af6d26c33175c1c4
SalahSoussou <soussousalahiddine@gmail.com>
2024-10-05 19:33:27 +0100

    ok

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -17,6 +17,6 @@ class BankAccount:
             return False
 
     def display_balance(self):
-        print(f"Current Balance: ${float(self.account_balance)}")
+        print("Current Balance: $%{:.2f}".format(self.account_balance))
 
 
