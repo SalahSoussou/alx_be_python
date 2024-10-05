fb337c9ef4c40cb3c2c5837562c8d3dd166277b2
SalahSoussou <soussousalahiddine@gmail.com>
2024-10-05 19:18:35 +0100

    BankAccount

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,22 @@
+
+
+class BankAccount():
+    def __init__(self,account_balance) :
+        self.account_balance = account_balance
+        pass
+
+    def deposit(self,amount):
+        self.account_balance = self.account_balance + amount
+        return self.account_balance
+
+
+    def withdraw(self,amount):
+        if self.account_balance - amount >= 0:
+            return True
+        elif self.account_balance - amount < 0:
+            return False
+
+    def display_balance(self):
+        print(self.account_balance)
+
+
